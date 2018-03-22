d9627b905bdd35689c096d006d7a2dfd80a7980e
Ken Wong <ken.wong@solnet.co.nz>
2018-03-23 08:30:20 +1300

    initial

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using App1.Views;
+using Xamarin.Forms;
+using XamlCSS;
+using XamlCSS.XamarinForms;
+
+namespace App1
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+            Console.WriteLine("=====> " + typeof(App).Assembly.ToString());
+            Css.Initialize(this, new[] { typeof(App).Assembly });
+
+            InitializeComponent();
+
+            //this.Resources.Add("appStyleSheet", new StyleSheet { Content = "Button { FontAttributes: Italic;}" });
+            this.Resources.Add("appStyleSheet", new StyleSheet { Content = @"@import ""Resources/baseStyle.scss"";" });
+            //this.Resources.Add("appVariables", "$base-color: #00ff00;");
+
+            MainPage = new MainPage();
+        }
+
+        public string cssStyle = @"
+@import ""Resources/baseStyle.scss"";
+";
+
+        public string cssStyle2 = @"
+$base-color: #00ff00;
+
+Button {
+    BackgroundColor: Red;
+    TextColor: White;
+}
+";
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ App1/App1/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamlCSS.CssParsing;
+using XamlCSS.XamarinForms;
+
+namespace App1.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+            /*
+             * this is setting css programatically
+             */
+            //var app = Application.Current as App;
+            //Css.SetStyleSheet(aboutPageTab, CssParser.Parse(app.cssStyle2));
+        }
+    }
+}
